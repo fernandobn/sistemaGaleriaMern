3fa44c3e91a96a5eaad38ec3b08a755b064538e0
fernandobn <154335390+fernandobn@users.noreply.github.com>
2025-02-08 17:08:59 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Usa el puerto proporcionado por Render, si está disponible
+    host: '0.0.0.0', // Asegura que Vite escuche en todas las interfaces de red
+  },
 })
