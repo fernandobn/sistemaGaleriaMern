c5c033a8de9dfb32f329226fd16e9f1bb86df3a7
fernandobn <154335390+fernandobn@users.noreply.github.com>
2025-02-08 17:52:46 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // URL desde la variable de entorno
+  baseURL: import.meta.env.VITE_API_URL, // Accede correctamente a la variable
   headers: {
     "Content-Type": "application/json",
   },
