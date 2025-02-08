bda859aa8073bec68c146dd6aec3ec8dd5e5a6c0
fernandobn <154335390+fernandobn@users.noreply.github.com>
2025-02-08 17:13:52 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
     port: process.env.PORT || 3000, // Usa el puerto proporcionado por Render, si está disponible
     host: '0.0.0.0', // Asegura que Vite escuche en todas las interfaces de red
   },
-})
+  preview: {
+    allowedHosts: ['sistemagaleriafotos.onrender.com'], // Agrega tu dominio de Render aquí
+  },
+});
